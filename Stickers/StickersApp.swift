4def4eadcc35b66edd2677d7200a29000ffad8b9
“larissa.ribeiro” <larissa.ribeiro@g.globo>
2024-08-23 17:42:16 -0300

    Initial Commit

---
+++ Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by Larissa Souza on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
